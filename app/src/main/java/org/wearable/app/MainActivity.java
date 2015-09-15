04e9b5280eaf7948280a3d3085004dc57f0c32fe
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2015-09-15 11:14:53 -0300

    using AsynTask to threads

--- app/src/main/java/org/wearable/app/MainActivity.java
+++ app/src/main/java/org/wearable/app/MainActivity.java
@@ -1,26 +1,17 @@
 package org.wearable.app;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIUtils;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,37 +23,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    makeRequest();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        t.start();
-    }
-
-    @SuppressLint("LongLogTag")
-    private void makeRequest() throws IOException, URISyntaxException {
-        URI url = URIUtils.createURI("http", "10.0.2.2", 4567,
-                "/methodPost", null, null);
-        HttpPost post = new HttpPost(url);
-
-        List params = new ArrayList();
-        params.add(new BasicNameValuePair("p1", "111"));
-        params.add(new BasicNameValuePair("p2", "222"));
-        post.setEntity(new UrlEncodedFormEntity(params));
-
-        DefaultHttpClient hc = new DefaultHttpClient();
-        HttpResponse response = hc.execute(post);
-
-        String responseString = EntityUtils.toString(response.getEntity());
-        Log.d("Resposta da requisicao feita", responseString);
+        doPost();
     }
 
     @Override
@@ -85,5 +46,33 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void doPost() {
+        AsyncTask<Void, Void, String> postTask = new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... voids) {
+                List params = new ArrayList();
+                params.add(new BasicNameValuePair("p1", "111"));
+                params.add(new BasicNameValuePair("p2", "222"));
+                HttpResponse response = null;
+
+                try {
+                    response = Web.doPost(params);
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+
+                String responseString = null;
+                try {
+                    responseString = EntityUtils.toString(response.getEntity());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return responseString;
+            }
+        };
+        postTask.execute();
     }
 }
