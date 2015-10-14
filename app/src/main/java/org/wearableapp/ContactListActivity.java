4a3637e98af3392110265351b4b6fd4127efafde
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-10-14 17:59:15 -0300

    Contact list

---
+++ app/src/main/java/org/wearableapp/ContactListActivity.java
@@ -0,0 +1,14 @@
+package org.wearableapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ContactListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+
+}
