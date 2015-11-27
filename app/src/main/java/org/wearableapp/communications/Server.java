97173929e7e919db2ecf702b4fc7635d1bc62b1e
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-11-27 16:09:54 -0200

    Default ip

--- app/src/main/java/org/wearableapp/communications/Server.java
+++ app/src/main/java/org/wearableapp/communications/Server.java
@@ -3,7 +3,7 @@ package org.wearableapp.communications;
 public abstract class Server {
     public static final String SCHEME_HTTP = "http";
     public static final String SCHEME_MQTT = "tcp";
-    public static final String HOST = " 10.0.2.2";
+    public static final String HOST = "10.0.2.2";
     public static final int PORT_HTTP = 4567;
     public static final int PORT_MQTT = 1883;
 }
